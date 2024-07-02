39ba59ef346870463b423c4918a9d9f25c090639
kran891 <kranthi8919@gmail.com>
2024-07-02 17:41:39 +0530

    padding

--- src/Components/Homepage/body-cards.tsx
+++ src/Components/Homepage/body-cards.tsx
@@ -8,14 +8,14 @@ const BodyCards: React.FC<Props> = ({ data, width }) => {
     const w=`sm:w-[${width}]`
     
     return <>
-        <div className="sm:flex grid grid-cols-2 pb-2 *:shadow-lg  *:shrink-0 overflow-x-auto *:overflow-hidden gap-[1.5%]">
+        <div className="sm:flex grid grid-cols-2  pb-2 *:shadow-lg  *:shrink-0 overflow-x-auto *:overflow-hidden gap-[1.5%]">
             {data.map(ele => {
-                return <a className={`${w} rounded-2xl`} href={ele.url}><div className="relative">
+                return <a className={`group sm:w-[25%]  rounded-2xl hover:bg-orange-200`} href={ele.url}><div className="relative group-hover:bg-orange-200">
                     <img src={ele.imgageUrl} className="h-[150px] w-full object-cover" alt={ele.header} />
-                    <div className="mt-2 mx-1 pb-2">
-                        <h2 className="font-semibold text-center">{ele.header}</h2>
+                    <div className="mt-2 mx-1 pb-2 group-hover:bg-orange-200 align-bottom">
+                        <h2 className="font-semibold text-center">{ele.header.length>25?ele.header.substring(0,25)+"...":ele.header}</h2>
 
-                        <div className={`flex justify-between items-end m-2 mt-10`}>
+                        <div className={`flex justify-between  items-end mt-2`}>
                             <div>
                                 <p className="text-gray-400 text-[12px]">{ele.reviews} Reviews</p>
                                 <p className="flex"><span className="flex">{[...Array(ele.rating)].map((_, i) => {
@@ -27,7 +27,8 @@ const BodyCards: React.FC<Props> = ({ data, width }) => {
                                 </p>
                             </div>
                             <div className="text-right">
-                                <p className="text-[12px] text-gray-400">Per Person from</p>
+                                
+                                {ele.offerPrice ?<p className="text-[12px] text-gray-400">Per Person from</p>:<p className="text-[12px] pt-[12px] text-gray-400">Per Person from</p>}
                                 <p className="font-bold text-sm">AED {ele.offerPrice || ele.actualPrice}</p>
                                 {ele.offerPrice && <p className="line-through text-[12px] text-gray-300">AED{ele.actualPrice}</p>}
                             </div>
