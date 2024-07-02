a534a487b386e468c87ebc43948f5d1893d3d119
kran891 <kranthi8919@gmail.com>
2024-07-02 12:22:17 +0530

    card

--- src/Components/Homepage/city-card.tsx
+++ src/Components/Homepage/city-card.tsx
@@ -4,14 +4,11 @@ import CityCards from './city-cards';
 import BodyCards from './body-cards';
 import { Events } from '../../constansts/events-const';
 interface CityCardProps {
-    heading: string;
-    description: string;
-    Card: typeof CityCards;
-    data: CityImage[];
+  heading: string;
+  description: string;
+  Card: typeof CityCards;
+  data: CityImage[];
 }
-<<<<<<< HEAD
-const Card:React.FC<Props> = ({heading,description,Card,data}) => {
-=======
 
 interface BodyCardProps {
   heading: string;
@@ -22,17 +19,12 @@ interface BodyCardProps {
 }
 
 const Card:React.FC<CityCardProps | BodyCardProps> = ({heading,description,Card,...rest}) => {
->>>>>>> 571e36f6fd1a5add8702ee908c85722605edd8cc
   return <>
   <div className='m-14 mt-4'>
     <h1 className='font-bold text-[20px]'>{heading}</h1>
     <p className='text-[#707081] text-[13px] pt-2 pb-3'>{description}</p>
-<<<<<<< HEAD
-    {Card && <Card  data={data}/>}
-=======
     { Card===CityCards && <CityCards data={(rest as CityCardProps).data}/>}
     {Card === BodyCards && <BodyCards data={(rest as BodyCardProps).data} width={(rest as BodyCardProps).width}/>}
->>>>>>> 571e36f6fd1a5add8702ee908c85722605edd8cc
   </div>
   </>
     
