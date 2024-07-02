d3ec0ab7d4a74c6124b61b1c9b262c39e52e64a6
kran891 <kranthi8919@gmail.com>
2024-07-02 16:39:07 +0530

    padding

--- src/Components/Homepage/body-cards.tsx
+++ src/Components/Homepage/body-cards.tsx
@@ -6,11 +6,11 @@ interface Props {
 }
 const BodyCards: React.FC<Props> = ({ data, width }) => {
     return <>
-        <div className="lg:flex grid grid-cols-2 *:shadow-lg *:shrink-0  overflow-x-scroll *:overflow-hidden gap-[1.5%]">
+        <div className="md:flex grid grid-cols-2 pb-2 *:shadow-lg  *:shrink-0 overflow-x-auto *:overflow-hidden gap-[1.5%]">
             {data.map(ele => {
-                return <a className={`w-full lg:w-[${width}] rounded-2xl`} href={ele.url}><div className="relative">
-                    <img src={ele.imgageUrl} className="h-[150px] w-full" alt={ele.header} />
-                    <div className="mt-2 mx-1 relative">
+                return <a className={`md:w-[${width}] rounded-2xl`} href={ele.url}><div className="relative">
+                    <img src={ele.imgageUrl} className="h-[150px] w-full object-cover" alt={ele.header} />
+                    <div className="mt-2 mx-1 pb-2">
                         <h2 className="font-semibold text-center">{ele.header}</h2>
 
                         <div className={`flex justify-between items-end m-2 mt-10`}>
